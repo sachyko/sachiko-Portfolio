047f4a1274c022dd9daf01efe22b712897c14d4b
SachikoPonpon <ponposachiko2@gmail.com>
2025-05-20 13:32:32 +0900

    edit the project list

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,11 +2,10 @@ import React from "react";
 
 import Hero from "../Hero/Hero";
 import CodeJourney from "../CodeJourney/CodeJourney";
-import ProjectsList from "../ProjectsList/ProjectsList";
+// import ProjectsList from "../ProjectsList/ProjectsList";
 import ProjectsCarousel from "../ProjectsCarousel/ProjectsCarousel";
 import Footer from "../Footer/Footer";
 
-import ravenousImg from "../App/pictures/ravenous.jpg";
 import homepage from "../App/pictures/homepage.jpg";
 import sweetCookies from "./sweet cookies.png";
 
@@ -21,15 +20,15 @@ const info = [
 		details:
 			"SweetStreet is a responsive cookie shop website I designed and developed to showcase a small homemade cookie business. The website features a stylish cookie catalog, an easy-to-use contact form for custom orders, and a warm, inviting design that reflects the homemade charm of the brand. Users can browse different cookie flavors, learn more about the business, and place custom order inquiries directly through the site.",
 	},
-	{
-		src: ravenousImg,
-		name: "Ravenous",
-		category: "Restaurant Recommendation App",
-		github: "https://github.com/sachyko/ravenous",
-		projectLink: "https://sachyko.github.io/ravenous/",
-		details:
-			"Ravenous is a restaurant recommendation application built using React, allowing users to search for restaurants based on their location. It retrieves data from the Yelp API and presents it in  easy-to-navigate interface, enabling users to filter restaurant recommendations based on the best match, highest rating or most reviews.",
-	},
+	// {
+	// 	src: ravenousImg,
+	// 	name: "Ravenous",
+	// 	category: "Restaurant Recommendation App",
+	// 	github: "https://github.com/sachyko/ravenous",
+	// 	projectLink: "https://sachyko.github.io/ravenous/",
+	// 	details:
+	// 		"Ravenous is a restaurant recommendation application built using React, allowing users to search for restaurants based on their location. It retrieves data from the Yelp API and presents it in  easy-to-navigate interface, enabling users to filter restaurant recommendations based on the best match, highest rating or most reviews.",
+	// },
 	{
 		src: homepage,
 		name: "My Language Friends",
